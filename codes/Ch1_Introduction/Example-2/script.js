d794aec546b174a2e30811b509fc4a66254c46b6
hegdeashwin <ashwin.hegde3@gmail.com>
2016-01-01 20:08:47 +0530

    Fixes typo + Updates intro eg2 with inline comments

--- codes/Ch1_Introduction/Example-2/script.js
+++ codes/Ch1_Introduction/Example-2/script.js
@@ -1,13 +1,26 @@
+/**
+ * The goal of this file is to provide the basic understanding of
+ * 1. Know about application object events
+ * 2. How to pass object to application start method
+ */
+
 (function() {
   'use strict';
 
   var app = Marionette.Application.extend({
+
+    /**
+     * The options parameter is passed throught the app while creating app instance
+     */
     initialize: function(options) {
       console.log("LOG: Marionette application got initialized");
       console.log("LOG: Application options - ", options);
     }
   });
 
+  /**
+   * Create a application object to store project meta info
+   */
   var AppOptions = {
     project: {
       codename: "my-app",
@@ -15,21 +28,41 @@
     }
   };
 
+  /**
+   * Pass the application object to app while creating app instance
+   */
   app = new app(AppOptions);
 
+  /**
+   * Application object subscribe few events, these events can be used to perform some
+   * additional task for the application say eg. pre processing some data etc.
+   *
+   * `before:start` gets automatically triggered just before the application start and before the initializers
+   * The options parameter is passed throught the start method of the application object
+   */
   app.on("before:start", function(options) {
     console.log("LOG: before:start application event got called");
     console.log("LOG: before:start options - ", options);
   });
 
+  /**
+   * `start` get automatically triggered after the application has started and after the initializers has been executed.
+   */
   app.on("start", function(options) {
     console.log("LOG: start application event got called");
     console.log("LOG: start options - ", options);
   });
 
+  /**
+   * Create a start object to store meta info
+   */
   var initConfigOptions = {
     apikey: "3a-6e-0c-ac-cc-0e-a9-94"
   };
+
+  /**
+   * Start the application with the initial start object.
+   */
   app.start(initConfigOptions);
 
 })();
