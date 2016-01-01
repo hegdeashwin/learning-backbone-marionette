06a38a354e128b8be64b5891217d4d6f184d2e7e
hegdeashwin <ashwin.hegde3@gmail.com>
2016-01-01 12:01:55 +0530

    Removes unwanted things from egs

--- codes/Ch5_Object/Example-1/script.js
+++ codes/Ch5_Object/Example-1/script.js
@@ -4,11 +4,6 @@
   var app = Marionette.Application.extend({});
   app = new app();
 
-  app.on("start", function(options) {
-    Backbone.history.start();
-    console.log("LOG: Backbone history is started!");
-  });
-
   var MyObject = Marionette.Object.extend({
 
     initialize: function() {
--- codes/Ch5_Object/Example-2/script.js
+++ codes/Ch5_Object/Example-2/script.js
@@ -4,12 +4,7 @@
   var app = Marionette.Application.extend({});
   app = new app();
 
-  app.on("start", function(options) {
-    Backbone.history.start();
-    console.log("LOG: Backbone history is started!");
-  });
-
-  var ProfileObj = Marionette.Object.extend({
+  var MyObject = Marionette.Object.extend({
 
     initialize: function(options) {
       console.log("LOG: Object got initialize");
@@ -18,24 +13,9 @@
 
   });
 
-  new ProfileObj({
+  new MyObject({
     "name": "Ashwin Hegde",
-    "job": "Software Engineer",
-    "techside": {
-      "servers": ["JavaScript", "Python", "Ruby", "Golang", "PHP"],
-      "clients": ["HTML", "CSS", "JavaScript", "LESS"],
-      "tasks": ["Grunt", "Gulp"],
-      "build": ["rjs", "browserify", "webpack"],
-      "tests": ["Mocha", "Jasmine", "QUnit"],
-      "search": ["Elasticsearch", "Solr"],
-      "frameworks": {
-        "JavaScript": ["Backbone", "Angular", "React", "Marionette", "Chaplin"],
-        "Python": ["Django"],
-        "Ruby": ["Rails"],
-        "Golang": ["Martini", "Gorilla"],
-        "PHP": ["Laravel", "CodeIgniter", "Yii", "CakePHP"]
-      }
-    }
+    "job": "Software Engineer"
   });
 
   app.start();
