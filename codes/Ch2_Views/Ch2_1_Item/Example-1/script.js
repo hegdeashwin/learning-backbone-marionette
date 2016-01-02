aa0939b94b52cbea3e23c2521ec9959811aa95df
hegdeashwin <ashwin.hegde3@gmail.com>
2016-01-02 20:45:48 +0530

    Updates ItemView egs with comments

--- codes/Ch2_Views/Ch2_1_Item/Example-1/script.js
+++ codes/Ch2_Views/Ch2_1_Item/Example-1/script.js
@@ -1,16 +1,35 @@
+/**
+ * The goal of this file is to provide the basic understanding of
+ * 1. How to create ItemView
+ * 2. initialized ItemView constructor
+ */
+
 (function() {
   'use strict';
 
   var app = Marionette.Application.extend({});
   app = new app();
-  app.start();
 
+  /**
+   * Create an ItemView extending Marionette.ItemView.
+   * An `ItemView` is a single item which may be represented as a Backbone Model or Collection.
+   */
   var MyView = Marionette.ItemView.extend({
+
+    /**
+     * `initialize` is a constructor function which gets called automatically once the instance is created
+     */
     initialize: function() {
       console.log("LOG: ItemView got initialize");
     }
+
   });
 
+  /**
+   * Creating MyView instance using `new`
+   */
   new MyView();
+
+  app.start();
 
 })();
--- codes/Ch2_Views/Ch2_1_Item/Example-2/script.js
+++ codes/Ch2_Views/Ch2_1_Item/Example-2/script.js
@@ -1,27 +1,44 @@
+/**
+ * The goal of this file is to provide the basic understanding of
+ * 1. How to use el and defining scope
+ * 2. Binding DOM event with eventhash
+ */
+
 (function() {
   'use strict';
 
   var app = Marionette.Application.extend({});
   app = new app();
-  app.start();
 
   var MyView = Marionette.ItemView.extend({
-    initialize: function() {
-      console.log("LOG: ItemView got initialize");
-    },
 
-    el: "#container",
+    /**
+     * el can be a DOM selector string or an element;
+     * otherwise it will be created from `tagName`, `className`, `id` and `attributes` properties
+     * if `el` is not set then el will be empty `div`
+     *
+     * It also defines the scope, events can be binded
+     */
+    el: "#first-container",
 
+    /**
+     * `events` or eventshash can be used to specify a set of DOM events that will be
+     * mapped to methods on the view (el).
+     *
+     * click event is binded to DOM element having class .showHelloMsg within "#first-container" el and not outside
+     */
     events: {
       "click .showHelloMsg": "showHelloMsg"
     },
 
     showHelloMsg: function() {
-      console.log("LOG: Hello World");
+      console.log("LOG: Hello World!");
     }
 
   });
 
   new MyView();
+
+  app.start();
 
 })();
