414ce17645e730c2b5ae87cf10ee2be537dce10b
hegdeashwin <ashwin.hegde3@gmail.com>
2016-01-03 21:15:15 +0530

    Updates comments for Regions and Layout egs

--- codes/Ch2_Views/Ch2_2_Regions_and_Layout/Example-2/script.js
+++ codes/Ch2_Views/Ch2_2_Regions_and_Layout/Example-2/script.js
@@ -1,33 +1,98 @@
+/**
+ * The goal of this file is to provide the basic understanding of
+ * 1. How to render templates into respected ItemView el instead of default div
+ * 2. How to bind respected DOM events binding
+ */
+
 (function() {
   'use strict';
 
   var app = Marionette.Application.extend({});
   app = new app();
 
+  /**
+   * Creates Header Item view to hold header-template including header events binding
+   * As `el` reference is provided #header-template will be rapped by respected el
+   */
   var HeaderView = Marionette.ItemView.extend({
-    el: ".headerMenu",
+    el: ".headerSection",
 
-    template: "#header-template"
+    template: "#header-template",
+
+    /**
+     * Header elements events binding
+     */
+    events: {
+      "click button": "detectHeader"
+    },
+
+    detectHeader: function() {
+      console.log("LOG: This is header section");
+    }
+
   });
 
+  /**
+   * Creates Content Item view to hold content-template including content events binding
+   * As `el` reference is provided #content-template will be rapped by respected el
+   */
   var ContentView = Marionette.ItemView.extend({
-    el: ".contentMenu",
+    el: ".contentSection",
 
-    template: "#content-template"
+    template: "#content-template",
+
+    /**
+     * Content elements events binding
+     */
+    events: {
+      "click button": "detectContent"
+    },
+
+    detectContent: function() {
+      console.log("LOG: This is content section");
+    }
+
   });
 
+  /**
+   * Creates Footer Item view to hold footer-template including footer events binding
+   * As `el` reference is provided #footer-template will be rapped by respected el
+   */
   var FooterView = Marionette.ItemView.extend({
-    el: ".footerMenu",
+    el: ".footerSection",
 
-    template: "#footer-template"
+    template: "#footer-template",
+
+    /**
+     * Footer elements events binding
+     */
+    events: {
+      "click button": "detectFooter"
+    },
+
+    detectFooter: function() {
+      console.log("LOG: This is footer section");
+    }
+
   });
 
+  /**
+   * Creates Master LayoutView hold container-template which includes header, section and footer
+   */
   var MasterLayoutView = Marionette.LayoutView.extend({
 
     el: '#main-container',
 
+    /**
+     * This will act as parent to above ItemViews
+     */
     template: "#container-template",
 
+    /**
+     * Divided the "container-template" view in to logic regions;
+     * As the container-template has header, section and footer as an element, so just assign it as value
+     * it can be selector or elements; The key will be used down below to use show method
+     */
     regions: {
       header: "header",
       section: "section",
@@ -36,9 +101,16 @@
 
   });
 
+  /**
+   * Create an instance of MasterLayoutView using `new` and call render method to get the container-template
+   * and its regions
+   */
   var masterLayout = new MasterLayoutView();
-  masterLayout.render().el;
+  masterLayout.render();
 
+  /**
+   * Use instance regions show method and pass instance of respected ItemView
+   */
   masterLayout.header.show(new HeaderView());
   masterLayout.section.show(new ContentView());
   masterLayout.footer.show(new FooterView());
